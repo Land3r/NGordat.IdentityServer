51ae0dea3023fca225cd7c0f10be85729d2af98a
Land3r <nicolas.gordat@gmail.com>
2023-03-14 12:02:25 +0100

    Fix

--- src/NGordat.IdentityServer.STS/Scripts/site.ts
+++ src/NGordat.IdentityServer.STS/Scripts/site.ts
@@ -1,11 +1,3 @@
-import { getCookie, setCookie } from 'typescript-cookie'
-
-function setApplicationTheme(themeName: string) {
-    // The following value must match ThemeHelpers.CookieThemeKey
-    const cookieName: string = "Application.Theme"
-    setCookie(cookieName, themeName);
-}
-
 function toggleReveal(elementId: string, revealBtnId: string) {
     const iconOn: string = "io-eye";
     const iconOff: string = "io-eye-off";
@@ -23,4 +15,63 @@ function toggleReveal(elementId: string, revealBtnId: string) {
         iconElm.classList.toggle(iconOn);
         iconElm.classList.toggle(iconOff);
     }
+}
+
+type Options = {
+    expires?: Date | number | string,
+    path?: string,
+    domain?: string,
+    secure?: boolean,
+}
+
+class Cookie {
+    static set(name: string, value: string, options?: Options) {
+        options = options || {};
+
+        let expires = options.expires;
+
+        if (typeof expires == "number" && expires) {
+            let d = new Date();
+            d.setTime(d.getTime() + expires * 1000);
+            expires = options.expires = d;
+        }
+        if (expires && (<Date>expires).toUTCString) {
+            options.expires = (<Date>expires).toUTCString();
+        }
+
+        value = encodeURIComponent(value);
+
+        let updatedCookie = name + "=" + value;
+
+        let propName: keyof Options;
+        for (propName in options) {
+            updatedCookie += "; " + propName;
+            let propValue = options[propName];
+            if (propValue !== true) {
+                updatedCookie += "=" + propValue;
+            }
+        }
+
+        document.cookie = updatedCookie;
+    }
+
+    static get(name: string): string {
+        let matches = document.cookie.match(
+            new RegExp("(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"));
+        return matches ? decodeURIComponent(matches[1]) : '';
+    }
+}
+
+function setCookie(name: string, value: string, options?: Options) {
+    Cookie.set(name, value, options)
+}
+
+function getCookie(name: string) {
+    return Cookie.get(name);
+}
+
+function deleteCookie(name: string) {
+    setCookie(name, "", {
+        expires: -1
+    })
 }
--- src/NGordat.IdentityServer.STS/wwwroot/js/site.js
+++ src/NGordat.IdentityServer.STS/wwwroot/js/site.js
@@ -1,26 +1,60 @@
-define("site", ["require", "exports", "typescript-cookie"], function (require, exports, typescript_cookie_1) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    function setApplicationTheme(themeName) {
-        // The following value must match ThemeHelpers.CookieThemeKey
-        var cookieName = "Application.Theme";
-        typescript_cookie_1.setCookie(cookieName, themeName);
+function toggleReveal(elementId, revealBtnId) {
+    var iconOn = "io-eye";
+    var iconOff = "io-eye-off";
+    var inputElm = document.getElementById(elementId);
+    var iconElm = document.getElementById(revealBtnId);
+    if (inputElm.type == 'password') {
+        inputElm.type = 'text';
+        iconElm.classList.toggle(iconOn);
+        iconElm.classList.toggle(iconOff);
     }
-    function toggleReveal(elementId, revealBtnId) {
-        var iconOn = "io-eye";
-        var iconOff = "io-eye-off";
-        var inputElm = document.getElementById(elementId);
-        var iconElm = document.getElementById(revealBtnId);
-        if (inputElm.type == 'password') {
-            inputElm.type = 'text';
-            iconElm.classList.toggle(iconOn);
-            iconElm.classList.toggle(iconOff);
-        }
-        else if (inputElm.type == 'text') {
-            inputElm.type = 'password';
-            iconElm.classList.toggle(iconOn);
-            iconElm.classList.toggle(iconOff);
-        }
+    else if (inputElm.type == 'text') {
+        inputElm.type = 'password';
+        iconElm.classList.toggle(iconOn);
+        iconElm.classList.toggle(iconOff);
     }
-});
+}
+var Cookie = /** @class */ (function () {
+    function Cookie() {
+    }
+    Cookie.set = function (name, value, options) {
+        options = options || {};
+        var expires = options.expires;
+        if (typeof expires == "number" && expires) {
+            var d = new Date();
+            d.setTime(d.getTime() + expires * 1000);
+            expires = options.expires = d;
+        }
+        if (expires && expires.toUTCString) {
+            options.expires = expires.toUTCString();
+        }
+        value = encodeURIComponent(value);
+        var updatedCookie = name + "=" + value;
+        var propName;
+        for (propName in options) {
+            updatedCookie += "; " + propName;
+            var propValue = options[propName];
+            if (propValue !== true) {
+                updatedCookie += "=" + propValue;
+            }
+        }
+        document.cookie = updatedCookie;
+    };
+    Cookie.get = function (name) {
+        var matches = document.cookie.match(new RegExp("(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"));
+        return matches ? decodeURIComponent(matches[1]) : '';
+    };
+    return Cookie;
+}());
+function setCookie(name, value, options) {
+    Cookie.set(name, value, options);
+}
+function getCookie(name) {
+    return Cookie.get(name);
+}
+function deleteCookie(name) {
+    setCookie(name, "", {
+        expires: -1
+    });
+}
 //# sourceMappingURL=site.js.map
